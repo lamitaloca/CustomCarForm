3870321ecc7e59562f0f22c16041c6f1e3e10fbe
lamitaloca <27406645-lamitaloca@users.noreply.replit.com>
2023-10-13 00:15:15 +0000

    Initial commit

---
+++ custom car form.py
@@ -0,0 +1,64 @@
+# Prints a line of equal signs
+print("======================")
+# Prints Welcome to UMBC Car Customization Form
+print("Welcome to the UMBC")
+print("Car customization Form!")
+#Prints a line of equal signs
+print("======================")
+# Inform user of multiple choice options
+print("For Multiple Choice Answers, Enter the Letter Next to the Answer")
+print()
+# Choose Make and Model of car
+print("~ Make & Model ~")
+print()
+print("1. What Model of Car are you ordering?")
+print("  a. Toyota Camry")
+print("  b. Honda Civic")
+print("  c. Tesla")
+print("  d. Pinto")
+# Inform user to select option
+letter = input("please enter 'a' - 'd':")
+print()
+# Ask if user wants an upgrade
+print("2.  Would you like to upgrade to leather seats?")
+answer = input("Please enter 'yes' or 'no': ")
+print()
+# Ask user for exterior color choice
+print("~ Exterior Color ~")
+print()
+print("3. What color would you like your car to be?")
+answer = input("You may enter the name of any color you would like: ")
+print()
+# Ask if user wants to upgrade to wheel package
+print("4. Would you like the deluxe wheel package?")
+answer = input("Please enter 'yes' or 'no': ")
+print()
+# Ask user what engine size they would like
+print("~ Interior ~")
+print("5. What size engine would you like?")
+print("  a. V6")
+print("  b. V8")
+print("  c. Hybrid")
+print("  d. Electric")
+print()
+# Ask user to choose option 
+letter = input("please enter 'a' - 'd':")
+print()
+# Ask if user would like heated seats
+print("6. Would you like heated seats?")
+print()
+# Ask user to answer yes or no
+answer = input("Please enter 'yes' or 'no':")
+print()
+# print equal sign banner
+print("=====================")
+print()
+# print summary of your answers
+print(" ~ Summary ~ ")
+print()
+print("Model Option : c")
+print("Leather Seats upgrade : yes")
+print("Exterior Color : White")
+print("Deluxe Wheel Package Upgrade: yes")
+print("Engine Size: d")
+print("Heated Seats Upgrade: yes")
